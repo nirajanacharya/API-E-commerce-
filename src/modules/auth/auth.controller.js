a8fdd55fe09732f50439d70a5c44a7046fb82ca2
Nirajan Acharya <nirajan.acharya666@gmail.com>
2024-11-28 09:26:28 +0545

    Add MongoDB database/ register

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -9,6 +9,10 @@ class AuthController {
 
             let data = await authService.transformUserCreateData (req);
 
+            //store user data in database
+
+            await authService.createUser(data);
+
             res.status(201).json({
                 data:{data},
                 message: 'User registered successfully',
